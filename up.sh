e9d0df39d012a3e07510b95573a96eda1ef605aa
Bram Ruttens <mail@bramruttens.be>
2023-02-17 23:20:06 +0100

    Add routing for HTTP(S) through VPN

--- up.sh
+++ up.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Route HTTP(S) traffic via VPN
+iptables -I OUTPUT -p tcp --dport 80 -j MARK --set-mark 1
+iptables -I OUTPUT -p tcp --dport 443 -j MARK --set-mark 1
+ip rule add fwmark 1 table 100
+ip route add default via $(ip route show 0.0.0.0/0 dev tun0 | awk '{print $3}') table 100
+
 # Start Tinyproxy
 tinyproxy -c /proxy/tinyproxy.conf -d
