231efa3b11c62a32efa6bfd8628a9025c946cf0c
Bram Ruttens <mail@bramruttens.be>
2023-02-17 23:25:58 +0100

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# Route HTTP(S) traffic via VPN
-iptables -I OUTPUT -p tcp --dport 80 -j MARK --set-mark 1
-iptables -I OUTPUT -p tcp --dport 443 -j MARK --set-mark 1
-ip rule add fwmark 1 table 100
-ip route add default via $(ip route show 0.0.0.0/0 dev tun0 | awk '{print $3}') table 100
+# Add a default route to the VPN gateway
+ip route add default via $(ip route show 0.0.0.0/0 dev tun0 | awk '{print $3}')
+
+# Redirect all traffic to the VPN gateway
+iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+
+# Drop all other traffic that is not going through the VPN
+iptables -I FORWARD -i eth0 ! -o tun0 -j DROP
 
 # Start Tinyproxy
 tinyproxy -c /proxy/tinyproxy.conf -d
