001422832f53a59f5f523bd1e1fd0f361546d561
Bram Ruttens <mail@bramruttens.be>
2023-02-18 13:52:39 +0100

    add tun0 to tinyproxy

--- up.sh
+++ up.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
-# Add a default route to the VPN gateway
-ip route add default via $(ip route show 0.0.0.0/0 dev tun0 | awk '{print $3}')
-
-# Redirect all traffic to the VPN gateway
-iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-
-# Drop all other traffic that is not going through the VPN
-iptables -I FORWARD -i eth0 ! -o tun0 -j DROP
-
 # Start Tinyproxy
 tinyproxy -c /proxy/tinyproxy.conf -d
